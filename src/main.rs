21e71e11f557407931936d3627d63f84d1b255da
LeoDog896 <LeoDog896@hotmail.com>
2022-10-06 17:14:38 +0000

    random word

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use memorable_wordlist::WORDS;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    println!("{:?}", WORDS.choose(&mut rng));
 }
